8981af81489737b9148fdd4811f19b179b09c4c2
EhsanShakil <ahsanshakeel1994@gmail.com>
2021-01-26 23:04:45 +0500

    creating header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "../components/Header/Header";
 
 function index() {
-  return <h1>Gatsby Blog Website</h1>;
+  return <Header />;
 }
 
 export default index;
