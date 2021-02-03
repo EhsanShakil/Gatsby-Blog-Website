72288b3bc6a2a526778cccd56eeeed0842cb8bf7
ehsanshakil <ahsanshakeel1994@gmail.com>
2021-02-03 23:55:56 +0500

    Updating

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,46 @@
 import React from "react";
+import Layout from "../components/Layout/Layout";
+import { graphql } from "gatsby";
 
-function blog() {
+function blog({ data }) {
+  const repsonse = data.allContentfulBlogPost.edges;
+  console.log(data.allContentfulBlogPost.edges);
+
   return (
-    <div>
-      <h1>Blog Post</h1>
-    </div>
+    <Layout>
+      <div>
+        <h1>Blog Post</h1>
+        <h2>
+          {repsonse.map((blog, index) => (
+            <div key={index}>
+              <h2>{blog.node.title}</h2>
+            </div>
+          ))}
+        </h2>
+      </div>
+    </Layout>
   );
 }
 
 export default blog;
+
+export const data = graphql`
+  query {
+    allContentfulBlogPost {
+      edges {
+        node {
+          title
+          slug
+          id
+          publicationDate(formatString: "")
+          excerpt {
+            excerpt
+          }
+          body {
+            raw
+          }
+        }
+      }
+    }
+  }
+`;
