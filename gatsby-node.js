81069e8cecb2933a2f141db76ec7947c5119d94c
ehsanshakil <ahsanshakeel1994@gmail.com>
2021-02-03 23:35:51 +0500

    Updating

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,39 +1,25 @@
-// exports.createPages = async ({ graphql, actions }) => {
-//   const { createPage } = actions;
-//   const result = await graphql`
-//     query MyQuery {
-//       allContentfulBlogPost {
-//         nodes {
-//           title
-//           content {
-//             raw
-//           }
-//         }
-//       }
-//     }
-//   `;
+const path = require("path");
 
-//   const data = result.data.allContentfulBlogPost.nodes;
-//   data.map((details) => {
-//     createPage({
-//       path: "blogs",
-//       component: require.resolve(`./src/templates/blog-post.js`),
-//       context: {
-//         data: details.title,
-//       },
-//     });
-//   });
-// };
-
-exports.createPages = async function ({ actions }) {
-  actions.createPage({
-    path: "my-dynamic-page",
-    component: require.resolve(`./src/templates/dynamic-page.js`),
-    context: {
-      // Data passed to context is available
-      // in pageContext props of the template component
-      name: "gatsby nodejs file",
-    },
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const response = await graphql(`
+    query {
+      allContentfulBlogPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `);
+  response.data.allContentfulBlogPost.edges.forEach((edge) => {
+    createPage({
+      path: `/blog/${edge.node.slug}`,
+      component: path.resolve("./src/templates/blog-post.tsx"),
+      context: {
+        slug: edge.node.slug,
+      },
+    });
   });
-  console.log("End of Gatsby Node File");
 };
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default ({ pageContext }) => {
-  // console.log(pageContext.title);
+  console.log(pageContext.data);
   return (
     <div>
       <div>Node</div>
